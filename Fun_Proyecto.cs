287eb4f8d3ad33231379fb806c83bb4754053ec3
marioxe301 <32968258+marioxe301@users.noreply.github.com>
2019-02-03 23:31:32 -0600

    cambios menores

--- Fun_Proyecto.cs
+++ Fun_Proyecto.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Proyecto_TB2
 {
+    
     class Fun_Proyecto
     {
+        string Usuario;
+        string Contraseña;
+        string Esquema;
+        string NConeccion;
+
+        Fun_Proyecto(string u,string c,string e,string Nc)
+        {
+            this.Usuario = u;
+            this.Contraseña = c;
+            this.Esquema = e;
+            this.NConeccion = Nc;
+        }
+
+        Fun_Proyecto(string u, string c)
+        {
+            this.Usuario = u;
+            this.Contraseña = c;
+        }
+
+        Fun_Proyecto() { }
+
+
+
+
+
+
     }
 }
